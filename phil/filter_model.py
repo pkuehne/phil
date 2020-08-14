e3391d21ed0bf5b4cec44fe8342a05f265d3bef5
Peter Kuehne <Peter.Kuehne@Gmail.com>
2020-08-14 16:23:36 +0100

    Add filtering logic for FilterModel

--- phil/filter_model.py
+++ phil/filter_model.py
@@ -3,6 +3,7 @@
 from PyQt5.QtCore import QSortFilterProxyModel
 from PyQt5.QtCore import Qt
 from PyQt5.QtCore import QModelIndex
+from phil.photo_model import PhotoModel
 
 # from phil.models.task_matcher import TaskMatcher
 
@@ -10,10 +11,26 @@ from PyQt5.QtCore import QModelIndex
 class FilterModel(QSortFilterProxyModel):
     """ Filters the input TreeModel to a table model of only the tasks """
 
-    # def __init__(self):
-    #     super(FilterModel, self).__init__()
-    # self.task_matcher = TaskMatcher()
-    # self.task_matcher.filters_updated.connect(self.invalidateFilter)
+    def __init__(self):
+        super(FilterModel, self).__init__()
+        self.person_filter = ""
+
+    def set_person_filter(self, person: str):
+        """ Sets the person to filter on """
+        self.person_filter = person
+        self.invalidate()
+
+    def index_has_person(self, index: QModelIndex):
+        """ Test whether the given index has a person link that we're filtering for """
+        if self.person_filter == "":
+            return True
+
+        persons = index.siblingAtColumn(PhotoModel.Columns.PERSONS).data(Qt.EditRole)
+
+        for person in persons:
+            if person.name == self.person_filter:
+                return True
+        return False
 
     def filterAcceptsRow(
         self, row, parent=QModelIndex()
@@ -24,25 +41,5 @@ class FilterModel(QSortFilterProxyModel):
         if not index.isValid():
             return False
 
-        return True
+        return self.index_has_person(index)
         # return self.task_matcher.match(node.data, node.parent.data)
-
-    # def setSourceModel(self, model):  # pylint: disable=invalid-name
-    #     """ Connect to source model signals """
-    #     super().setSourceModel(model)
-    #     self.sourceModel().modelReset.connect(self.invalidate)
-    #     self.sourceModel().dataChanged.connect(self.invalidate)
-
-    # def headerData(
-    #     self, section, orientation, role
-    # ):  # pylint: disable=invalid-name, no-self-use
-    #     """ Set the header information """
-    #     if orientation == Qt.Horizontal and role == Qt.DisplayRole and section == 0:
-    #         return "Task Source"
-    #     return self.sourceModel().headerData(section, orientation, role)
-
-    # def data(self, proxy_index, role=Qt.DisplayRole):  # pylint: disable= no-self-use
-    #     """ Return the data associated with the specific index for the role """
-    #     if role == Qt.DecorationRole:
-    #         return None
-    #     return self.sourceModel().data(self.mapToSource(proxy_index), role)
